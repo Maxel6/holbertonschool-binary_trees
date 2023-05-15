8063491610cc732a9f253b8e7f96330b7eb5d0a4
samdaphbynet <lj8@hotmail.fr>
2023-05-15 14:50:34 +0200

    update function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-<<<<<<< HEAD
 #include <stddef.h>
 
 /**
@@ -10,26 +9,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height;
+	size_t right_height;
+
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL)) {
 		return (0);
 	}
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
-=======
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t height = 0;
-
-	if (tree)
-	{
-		if (tree->left)
-			height++;
-		if (tree->right)
-			height++;
-	}
-	return (height);
->>>>>>> d815bfd0dd1d978667ff3fcc3a505638a725f038
 }
