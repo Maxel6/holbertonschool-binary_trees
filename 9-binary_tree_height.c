bd144afcf9fdcf176e85642863ba3e85263e399a
Axel Malka <axelmalka@outlook.fr>
2023-05-15 15:38:13 +0000

    adding balance function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,10 +13,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
 		return (0);
-
+	}
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	if (right_height > left_height)
+		return (right_height + 1);
+	else
+		return (left_height + 1);
 }
