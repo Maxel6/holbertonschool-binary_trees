f5fcb12ad04f3eb41ae888bf88d01fdc7cd9d590
samdaphbynet <lj8@hotmail.fr>
2023-05-16 13:29:16 +0200

    added prototype

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int find_left(const binary_tree_t *tree);
+int isPerfect(const binary_tree_t *tree, int find);
+
 #endif
